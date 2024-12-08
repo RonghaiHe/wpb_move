2e38ed95178ca3a0b444f1d952f3c0c58b57d12e
wp0 <wp0@sysu.com>
2024-12-08 11:39:52 +0800

    feat: Add velocity control in launch and add an action

--- src/show/round.cpp
+++ src/show/round.cpp
@@ -15,17 +15,39 @@ int main(int argc, char** argv)
     ros::NodeHandle n;
     ros::Publisher velcmd_pub = n.advertise<geometry_msgs::Twist>("/cmd_vel",10);
     geometry_msgs::Twist vel_cmd;
-    vel_cmd.linear.x = 0;
-    vel_cmd.linear.y = 0;
+    
     vel_cmd.linear.z = 0;
     vel_cmd.angular.x = 0;
     vel_cmd.angular.y = 0;
+    
+    ros::NodeHandle n_param("~");
+    int v_x, v_y, v_az;
+    n_param.param<int>("v_x", v_x, 0);
+    n_param.param<int>("v_y", v_y, 0);
+    n_param.param<int>("v_az", v_az, 1);
 
-    ros::Rate r(10.0);
+    vel_cmd.linear.x = v_x;
+    vel_cmd.linear.y = v_y;
+    vel_cmd.angular.z = v_az;
+
+
+    ros::Rate r(1.0);
+    for(int i=0;i<4;++i)
+        r.sleep();
     while(ros::ok()){
-        vel_cmd.angular.z = 1;
         velcmd_pub.publish(vel_cmd);
         r.sleep();
+
+        // vel_cmd.linear.x = 0.5;
+        // vel_cmd.angular.z = 0;
+        // velcmd_pub.publish(vel_cmd);
+        // for(int i=0;i<3;++i)
+        //     r.sleep();
+        // vel_cmd.linear.x = 0;
+        // vel_cmd.angular.z = 1;
+        // velcmd_pub.publish(vel_cmd);
+        // for(int i=0;i<3;++i)
+        //     r.sleep();
     }
     return 0;
 }
